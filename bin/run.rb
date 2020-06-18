f56ca60c816378eaa202bfed4b116aba37d6ea50
Diwo777 <diego.zegarra79@icloud.com>
2020-06-18 13:05:40 -0400

    update

--- bin/run.rb
+++ bin/run.rb
@@ -3,18 +3,9 @@ require_relative '../lib/command_line_interface'
 
 welcome
 login_or_setup
-# greet_user
-
 menu
 
-# api
 
-# workout = user_workout
-# recommend_exercises(workout)
 
-# add_exercise
-# create_workout
-
-# user interface
 # set dificulty level
 # keep track of created workout and exercises
--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -43,33 +43,6 @@ def login_or_setup
     end
 end
 
-# def user_log_in
-#     puts "1.Log in"
-#     puts "2.Register"
-#     input = gets.chomp()
-# end
-
-# def log_in
-#     puts "Enter username"
-#     username = gets.chomp()
-#     puts "Enter password"
-#     password = gets.chomp()
-#     log_in_validation(username, password)
-# end
-
-
-# def log_in_validation(username, password)
-#     # validates user return true/false
-# end
-
-# def register
-#     puts "What would be your user name"
-#     username = gets.chomp()
-#     puts "Create Password"
-#     password = gets.chomp()
-# end
-
-# prompts and returns the users level of fitness
 def user_level
     prompt = TTY::Prompt.new
     input = prompt.select(" Choose Your Level:", %w(Beginner Intermediate Advanced Pro))
@@ -88,6 +61,11 @@ def recommend_exercises(user_workout)
     list = workout.exercises.collect do |exercise|
         exercise.title
     end
+    bar = TTY::ProgressBar.new("Retrieving Workout... [:bar]", total: 30)
+        30.times do
+        sleep(0.1)
+        bar.advance(1)
+        end
 
     puts " Your recommended workouts are: #{list.join(', ')}"
 end
@@ -103,6 +81,11 @@ def add_exercise
     user_exits?(new_exercises)
     
     new_exercises = new_exercises.split(", ")
+    bar = TTY::ProgressBar.new("Saving exercises... [:bar]", total: 30)
+        30.times do
+        sleep(0.1)
+        bar.advance(1)
+        end
     add_user_exercise(workouts_to_edit, new_exercises)
 end
 
@@ -128,6 +111,11 @@ def add_workout
     user_exercises = gets.chomp
     user_exits?(user_exercises)
     user_exercises = user_exercises.split(", ")
+    bar = TTY::ProgressBar.new("Saving Workout... [:bar]", total: 30)
+        30.times do
+        sleep(0.1)
+        bar.advance(1)
+        end
 
     created_workout = Workout.create(title: user_workout)
 
@@ -153,6 +141,11 @@ def random_quote
     if json[random_number]["author"] == nil
         json[random_number]["author"] = "Someone Said This"
     end
+    bar = TTY::ProgressBar.new("Your life matters!... [:bar]", total: 30)
+        30.times do
+        sleep(0.1)
+        bar.advance(1)
+        end
 
     puts "\n\n\t\t\t\"#{json[random_number]["text"]}\"\n\n"
     puts "\t\t\t\t- #{json[random_number]["author"]}\n\n\n\n"
@@ -170,7 +163,7 @@ def browse_workouts_and_exercises
             puts "  #{ex}"
         end
 
-        # puts "  #{Workout.select(:title).map(&:title).uniq.join(", ")}"
+        
     elsif input == "Exercises"
         puts " Current list of Exercises:\n\n"
         exercises_array = Exercise.select(:title).map(&:title).uniq
@@ -178,36 +171,20 @@ def browse_workouts_and_exercises
             puts "  #{ex}"
         end
 
-        # puts "  #{exercises_array.join(", ")}"
     elsif input == "Users"
         puts " Current list of Users:\n\n"
         users_array = User.select(:username).map(&:username).uniq
         users_array.each do |user|
             puts "  #{user}"
         end
-
-        # puts "  #{exercises_array.join(", ")}"
     end
 end
 
 def menu
-    # pastel = Pastel.new
-    # puts pastel.cyan("Hello World")
-    # font = TTY::Font.new(:starwars)
-    # puts pastel.cyan(font.write("Welcome to Fitiron"))
-    # spinner = TTY::Spinner.new("[:spinner] Scribbling...", format: :pulse_2)
-    # spinner.auto_spin
-    # sleep(2)
-    # spinner.stop('Thanks for your using Fitiron!')
-    # bar = TTY::ProgressBar.new("Saving changes... [:bar]", total: 30)
-    # 30.times do 
-    #     sleep(0.1)
-    #     bar.advance(1)
-    # end
 
-    prompt_array = ["Get Workout\n", "Add Exercise\n", "Add Workout\n", "Browse Data\n", "Week Schedule\n", "Motivational Quote\n", "Exit\n"]
+    prompt_array = ["Get Workout\n", "Add Exercise\n", "Add Workout\n", "Browse Data\n", "Motivational Quote\n", "Exit\n"]
     prompt = TTY::Prompt.new
-    input = prompt.select("\n\n Main menu:\n", prompt_array) # %w(Get_Workout add_exercise add_workout look_week_schedule motivational_quote exit))
+    input = prompt.select("\n\n Main menu:\n", prompt_array) 
 
     case input.chop
     when "Get Workout"
