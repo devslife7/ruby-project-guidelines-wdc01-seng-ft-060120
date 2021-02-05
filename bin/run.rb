96db8d867d3a1f4cfc5640babdd980f828c21b4e
Marcos Velasco <devslife7@gmail.com>
2021-02-05 00:45:02 -0500

    removed dotenv gem and hid api key on relative file as a sample

--- bin/run.rb
+++ bin/run.rb
@@ -1,9 +1,11 @@
 require_relative '../config/environment'
 require_relative '../lib/command_line_interface'
+require_relative 'api_key'
 
 app = Fitiron_app.new
 
 app.welcome
+puts "This is my api key: #{API_KEY}"
 app.login_or_setup
 app.menu
 
